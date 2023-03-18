7aef48cb851adbc52d96f5842606a2c79d8a1b70
Max1323 <83382061+MaxiM219@users.noreply.github.com>
2023-03-18 17:37:38 -0300

    Agregado TPI

---
+++ TPI/src/PronosticoDeportivo/PronosticoDeportivo.java
@@ -0,0 +1,13 @@
+package PronosticoDeportivo;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class PronosticoDeportivo {
+
+	public static void main(String[] args) {
+		String rutaPronostico = "src\\Archivos\\pronostico.csv";
+		String rutaResultado = "src\\Archivos\\resultado.csv";
+	}
+
+}
